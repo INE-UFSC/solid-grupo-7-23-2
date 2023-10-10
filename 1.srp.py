2e947a5eef81ac6e09e8cd22d3356dd250caba20
Marcos Vinicius <marcos2005.machado@gmail.com>
2023-10-10 11:14:41 -0300

    Resolvi os exercícios 1 e 4

--- 1.srp.py
+++ 1.srp.py
@@ -6,6 +6,8 @@ ou
 Uma classe deve ter somente um motivo para mudar
 """
 
+#(Aqu classe possui 2 responsabilidades muito distintas: armazenar o nome do animal, e salvar outros animais no DB. Seria ideal usar 2 classes diferetes.)
+'''
 class Animal:
     def __init__(self, name: str):
         self.name = name
@@ -15,4 +17,29 @@ class Animal:
 
     # salva no DB
     def save(self, animal: Animal):
+        pass 
+
+'''
+
+
+
+#SOLUÇÃO
+
+class Animal:
+    def __init__(self, name: str):
+        self.name = name
+    
+    def get_name(self) -> str:
         pass
+
+class Animal_Saver:
+    def __init__(self) -> None:
+        pass
+
+    # salva no DB
+    def save(self, animal: Animal):
+        pass
+
+
+
+
--- 4.isp.py
+++ 4.isp.py
@@ -4,6 +4,10 @@ Crie interfaces que são específicas. Clientes não devem depender de interface
 """
 from abc import ABC
 
+#Aqui, ambas as classes concretas de janelas possuem métodos que nunca serão nescessários para ela, pelo fato de queles já pertencem a Interface.
+#O ideal seria termos interfaces diferentes para janelas com menu, e janelas com tamanho variável
+
+'''
 class IJanela(ABC):
     def maximizar(self):
         raise NotImplementedError
@@ -42,5 +46,34 @@ class JanelaSemMenu(IJanela):
     
     def fechar(self):
         pass
+'''
+#SOLUÇÃO
 
+class IJanelaComMenu(ABC):
+    def mostrar_menu(self):
+        raise NotImplementedError
+        
+    def fechar(self):
+        raise NotImplementedError
 
+class IJanelaTamanhoVariavel(ABC):
+    def maximizar(self):
+        raise NotImplementedError
+    
+    def minimizar(self):
+        raise NotImplementedError
+
+    def fechar(self):
+        raise NotImplementedError
+
+class JanelaTamanhoFixo(IJanelaComMenu):
+    def mostrar_menu(self):
+        pass
+    def fechar(self):
+        pass
+
+class JanelaSemMenu(IJanelaTamanhoVariavel):
+    def maximizar(self):
+        pass
+    def minimizar(self):
+        pass
