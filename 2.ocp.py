bd4a875edb411a96e0aff570286f2271e48fb2f9
Luuls <luan13467@hotmail.com>
2023-10-10 10:55:37 -0300

    fiz a primeira parte do arquivo 2

--- 2.ocp.py
+++ 2.ocp.py
@@ -3,28 +3,56 @@ Open-Closed Principle
 
 Classes devem estar fechadas para modificação, mas abertas para extensão
 """
-class Animal:
-    def __init__(self, name: str):
-        self.name = name
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self):
+        return
     
+    # def get_name(self) -> str:
+    #     pass
+
     def get_name(self) -> str:
+        return self.__class__.__name__
+
+    # def make_sound(self):
+    #     if self.name == 'lion':
+    #         print('roar')
+    #     elif self.name == 'mouse':
+    #         print('squeak')
+    #     else:
+    #         print('...')
+
+    @abstractmethod
+    def make_sound(self):
         pass
 
+
+class Lion(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+
     def make_sound(self):
-        if self.name == 'lion':
-            print('roar')
-        elif self.name == 'mouse':
-            print('squeak')
-        else:
-            print('...')
+        print('roar')
+
+
+class Mouse(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+
+    def make_sound(self):
+        print('squeak')
 
 animals = [
-    Animal('lion'),
-    Animal('mouse')
+    Lion(),
+    Mouse()
 ]
 
 def animal_sound(animals: list):
     for animal in animals:
+        print(animal.get_name())
         animal.make_sound()
 
 animal_sound(animals)
